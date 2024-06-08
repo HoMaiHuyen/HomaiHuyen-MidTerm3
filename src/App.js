3b2836331bd8ea834c5b71c7dd815e2a6ff191c9
HoMaiHuyen <huyen.homai25@student.passerellesnumeriques.org>
2024-06-08 19:24:18 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import './index.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/layout/Home';
 
